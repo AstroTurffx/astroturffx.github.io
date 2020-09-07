da597990f70f90d514a826fec4f5d089a7a09391
AstroTurffx <54775755+AstroTurffx@users.noreply.github.com>
2020-09-07 16:17:39 +0100

    Make the Buttons look better

--- js/filter.js
+++ js/filter.js
@@ -33,14 +33,3 @@ function RemoveClass(element, name) {
     }
     element.className = arr1.join(" ");
 }
-
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("BtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
